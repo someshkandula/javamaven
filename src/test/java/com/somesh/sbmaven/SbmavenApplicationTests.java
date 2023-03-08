f1012b857dff2263f66611f003333379a17cb351
somesh.kandula@gmail.com <somesh.kandula@gmail.com>
2023-03-08 23:55:56 +0530

    Maven Project Initial Commit!

---
+++ src/test/java/com/somesh/sbmaven/SbmavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somesh.sbmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
